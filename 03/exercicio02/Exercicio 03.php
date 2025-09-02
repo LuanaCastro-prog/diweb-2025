3a18b290c05ce6ae1d596fbc6300e46096dc4dbf
LuCastro <luanakcastro0511@gmail.com>
2025-09-02 10:51:00 -0300

    Add files via upload

---
+++ 03/exercicio02/Exercicio 03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercico 02</title>
+</head>
+<body>
+    <h1>titulo ou cabeçalho</h1>
+    <>
+        Parágrafo 01 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ut veritatis rem iure dolores alias odit eveniet dolorum natus, blanditiis neque debitis ad ducimus cumque id et, tempora quisquam quod!
+    <p>
+    Parágrafo 02 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt dicta ipsum at deleniti rerum nostrum sed sequi maxime a blanditiis? Sint harum quidem eius libero nihil minima quia dolorum officiis?
+    
+    </p>
+
+
+    <hr>
+    
+
+
---
+++ 03/exercicio02/Exercico 01.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=,initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <p>
+    cabeçalho tamanho 01
+    </p>    
+    </h1>
+
+    <h2>
+        <p>
+            cabeçalho tamanho 02
+        </p>
+    </h2>
+
+    <h3>
+        <p>
+            cabeclho tamanho 03
+        </p>
+    </h3>
+
+    <h4>
+        <p>
+            cabeçalho tamanho 04
+        </p>
+    </h4>
+
+    <h5>
+
+    <p>
+    cabeçalho tamanho 05
+    </p>
+    </h5>
+
+    <h6>
+    <p>
+        cabeçalho tamanho 06
+    </p>
+    </h6>
+</body>
+</html>
